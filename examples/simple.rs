8ad99089fa25a679b1b3e238545cc1702ee9a82a
Jovansonlee Cesar <ivanceras@gmail.com>
2016-10-31 01:49:29 +0800

    Convert into a library

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,19 +1,19 @@
-extern crate pulldown_cmark;
-extern crate svgbob;
+extern crate spongedown;
 
-use pulldown_cmark::Parser;
-use pulldown_cmark::Event;
-use pulldown_cmark::Tag;
 use std::fs::File;
 use std::error::Error;
 use std::io::Write;
 
+
 fn main(){
     let arg = r#"
 
-中文处理
+## Spongedown
 
-## Hello
+
+| 中文处理 | Data | CJK |
+|----------|------|-----|
+| 1        | 2    | 3   |
 
 ```bob
 
@@ -80,53 +80,8 @@ fn main(){
 ```
 
     "#;
-
-    let parser = Parser::new(arg);
-    let mut start_bob = false;
-    let mut bob_text = String::new();
-    let parser = parser.map(|event| match event {
-        Event::Text(text) => {
-            if start_bob {
-                bob_text.push_str(&text);
-                Event::Text("".into())
-            }else{
-                Event::Text(text)
-            }
-        }
-        Event::Start(tag) => {
-            match tag{
-                Tag::CodeBlock(ref lang) => {
-                    if lang == "bob" {
-                        start_bob = true;
-                        Event::Text("".into())
-                    }else{
-                        Event::Start(tag.clone())
-                    }
-                }
-                _ => Event::Start(tag)
-            }
-        }
-        Event::End(ref tag) => {
-            match *tag{
-                Tag::CodeBlock(ref lang) => {
-                    if lang == "bob" {
-                        start_bob = false;
-                        let svg = svgbob::to_svg(&bob_text).to_string();
-                        Event::Html(svg.into())
-                    }else{
-                        Event::End(tag.clone())
-                    }
-                }
-                _ => Event::End(tag.clone())
-            }
-        }
-        _ => event
-    });
-    let mut html = String::new();
-    pulldown_cmark::html::push_html(&mut html, parser);
-
+    let html = spongedown::parse(arg);
     save_to_file(&html);
-
 }
 
 fn save_to_file(html: &str)->Result<(),Box<Error>>{
---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+extern crate pulldown_cmark;
+extern crate svgbob;
+
+use pulldown_cmark::Parser;
+use pulldown_cmark::Event;
+use pulldown_cmark::Tag;
+use pulldown_cmark::Options;
+
+pub fn parse(arg: &str)->String{
+    let parser = Parser::new_ext(arg, Options::all());
+    let mut start_bob = false;
+    let mut bob_text = String::new();
+    let parser = parser.map(|event| match event {
+        Event::Text(text) => {
+            if start_bob {
+                bob_text.push_str(&text);
+                Event::Text("".into())
+            }else{
+                Event::Text(text)
+            }
+        }
+        Event::Start(tag) => {
+            match tag{
+                Tag::CodeBlock(ref lang) => {
+                    if lang == "bob" {
+                        start_bob = true;
+                        Event::Text("".into())
+                    }else{
+                        Event::Start(tag.clone())
+                    }
+                }
+                _ => Event::Start(tag)
+            }
+        }
+        Event::End(ref tag) => {
+            match *tag{
+                Tag::CodeBlock(ref lang) => {
+                    if lang == "bob" {
+                        start_bob = false;
+                        let svg = svgbob::to_svg(&bob_text).to_string();
+                        Event::Html(svg.into())
+                    }else{
+                        Event::End(tag.clone())
+                    }
+                }
+                _ => Event::End(tag.clone())
+            }
+        }
+        _ => event
+    });
+    let mut html = String::new();
+    pulldown_cmark::html::push_html(&mut html, parser);
+    html
+}
+
