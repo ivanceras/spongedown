357a57b631e3fe0ec5aa526f78711c0db3cea86b
Jovansonlee Cesar <ivanceras@gmail.com>
2017-03-31 22:46:25 +0800

    Added an idiomatic-y plugin system

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,43 @@
 extern crate pulldown_cmark;
 extern crate svgbob;
+extern crate comic;
 
 use pulldown_cmark::Parser;
 use pulldown_cmark::Event;
 use pulldown_cmark::Tag;
 use pulldown_cmark::Options;
+use std::collections::HashMap;
+use std::borrow::Cow;
 
-pub fn parse(arg: &str) -> String {
+fn bob_handler(s: &str) -> String {
+    svgbob::to_svg(s).to_string()
+}
+
+fn comic_handler(s: &str) -> String {
+    comic::to_svg(s).to_string()    
+}
+
+
+pub fn parse(arg: &str) -> String{
+    let mut plugins:HashMap<String, Box<Fn(&str)->String>>  = HashMap::new();
+    plugins.insert("bob".into(), Box::new(bob_handler));
+    plugins.insert("comic".into(), Box::new(comic_handler));
+    parse_with_plugins(arg, plugins)
+}
+
+
+
+pub fn parse_with_plugins(arg: &str, plugins: HashMap<String, Box<Fn(&str) -> String>>) -> String {
     let parser = Parser::new_ext(arg, Options::all());
-    let bob_fence = "bob";
-    let mut start_bob = false;
-    let mut bob_text = String::new();
+    let mut fence_start = false;
+    let mut text_buffer = String::new();
+    let mut active_handler: Option<&Box<Fn(&str)->String>> = None;
+    let mut active_fence: Option<String> = None;
+
     let parser = parser.map(|event| match event {
         Event::Text(text) => {
-            if start_bob {
-                bob_text.push_str(&text);
+            if fence_start {
+                text_buffer.push_str(&text);
                 Event::Text("".into())
             } else {
                 Event::Text(text)
@@ -23,12 +46,17 @@ pub fn parse(arg: &str) -> String {
         Event::Start(tag) => {
             match tag {
                 Tag::CodeBlock(ref lang) => {
-                    if lang == bob_fence {
-                        start_bob = true;
-                        bob_text.clear();
-                        Event::Text("".into())
-                    } else {
-                        Event::Start(tag.clone())
+                    match plugins.get(&lang.to_string()) {
+                        Some(handler) => {
+                            fence_start = true;
+                            text_buffer.clear();
+                            active_handler = Some(handler);
+                            active_fence = Some(lang.to_string());
+                            Event::Text("".into())
+                        }
+                        None => {
+                            Event::Start(tag.clone())
+                        }
                     }
                 }
                 _ => Event::Start(tag),
@@ -37,11 +65,20 @@ pub fn parse(arg: &str) -> String {
         Event::End(ref tag) => {
             match *tag {
                 Tag::CodeBlock(ref lang) => {
-                    if lang == bob_fence {
-                        start_bob = false;
-                        let svg = svgbob::to_svg(&bob_text).to_string();
-                        bob_text.clear();
-                        Event::Html(svg.into())
+                    if active_fence == Some(lang.to_string()) {
+                        fence_start = false;
+                        match active_handler{
+                            Some(handler) => {
+                                let out = handler(&text_buffer);
+                                text_buffer.clear();
+                                active_handler = None; //reset handler to none
+                                Event::Html(out.into())
+                            },
+                            None => {
+                                println!("Warning: no handler for this end tag");
+                                Event::Text("".into())
+                            }
+                        }
                     } else {
                         Event::End(tag.clone())
                     }
