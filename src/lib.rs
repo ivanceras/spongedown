e30530131121cd86482ff12e724da93c16901db5
Jovansonlee Cesar <ivanceras@gmail.com>
2017-04-27 14:16:54 +0800

    Completely remove pulldown-cmark

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,23 @@
 #![deny(warnings)]
-#![feature(custom_derive)]
-
-extern crate pulldown_cmark;
 #[macro_use]
 extern crate error_chain;
 
 extern crate svgbob;
 extern crate comic;
 extern crate csv;
-
-use pulldown_cmark::Parser;
-use pulldown_cmark::Event;
-use pulldown_cmark::Tag;
-use pulldown_cmark::Options;
+extern crate comrak;
+extern crate typed_arena;
+use typed_arena::Arena;
+use comrak::{parse_document, format_html, ComrakOptions};
+use comrak::nodes::{AstNode, NodeValue, NodeHtmlBlock};
 use std::collections::HashMap;
+use errors::*;
+
 mod errors {
     error_chain!{
-        errors {
-            PluginError{
-                description("Error in processing data to plugin")
-                display("Plugin error display!")
-            }
-        }
     }
 }
 
-use errors::*;
 
 
 /// convert bob ascii diagrams to svg
@@ -71,93 +63,15 @@ pub fn parse(arg: &str) -> String{
     plugins.insert("bob".into(), Box::new(bob_handler));
     plugins.insert("comic".into(), Box::new(comic_handler));
     plugins.insert("csv".into(), Box::new(csv_handler));
-    //parse_with_plugins(arg, plugins)
     parse_via_comrak(arg, &plugins)
 }
 
 
 
-pub fn parse_with_plugins(arg: &str, plugins: HashMap<String, Box<Fn(&str) -> Result<String>>>) -> String {
-    let parser = Parser::new_ext(arg, Options::all());
-    let mut fence_start = false;
-    let mut text_buffer = String::new();
-    let mut active_handler: Option<&Box<Fn(&str) -> Result<String>>> = None;
-    let mut active_fence: Option<String> = None;
 
-    let parser = parser.map(|event| match event {
-        Event::Text(text) => {
-            if fence_start {
-                text_buffer.push_str(&text);
-                Event::Text("".into())
-            } else {
-                Event::Text(text)
-            }
-        }
-        Event::Start(tag) => {
-            match tag {
-                Tag::CodeBlock(ref lang) => {
-                    match plugins.get(&lang.to_string()) {
-                        Some(handler) => {
-                            fence_start = true;
-                            text_buffer.clear();
-                            active_handler = Some(handler);
-                            active_fence = Some(lang.to_string());
-                            Event::Text("".into())
-                        }
-                        None => {
-                            Event::Start(tag.clone())
-                        }
-                    }
-                }
-                _ => Event::Start(tag),
-            }
-        }
-        Event::End(ref tag) => {
-            match *tag {
-                Tag::CodeBlock(ref lang) => {
-                    if active_fence == Some(lang.to_string()) {
-                        fence_start = false;
-                        match active_handler{
-                            Some(handler) => {
-                                let out = handler(&text_buffer);
-                                text_buffer.clear();
-                                active_handler = None; //reset handler to none
-                                match out{
-                                    Ok(out) => {
-                                        Event::Html(out.into())
-                                    },
-                                    Err(e)=> {
-                                        Event::Html(format!(r#"<pre class="error">{}</pre>"#,e).into())
-                                    }
-                                }
-                            },
-                            None => {
-                                println!("Warning: no handler for this end tag");
-                                Event::Text("".into())
-                            }
-                        }
-                    } else {
-                        Event::End(tag.clone())
-                    }
-                }
-                _ => Event::End(tag.clone()),
-            }
-        }
-        _ => event,
-    });
-    let mut html = String::new();
-    pulldown_cmark::html::push_html(&mut html, parser);
-    html
-}
-
-extern crate comrak;
-extern crate typed_arena;
 
 fn parse_via_comrak(arg: &str, plugins: &HashMap<String, Box<Fn(&str) -> Result<String>>>)->String{
 
-use typed_arena::Arena;
-use comrak::{parse_document, format_html, ComrakOptions};
-use comrak::nodes::{AstNode, NodeValue, NodeHtmlBlock};
 
     // The returned nodes are created in the supplied Arena, and are bound by its lifetime.
     let arena = Arena::new();
